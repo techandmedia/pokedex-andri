b9f5ffd3dd7530ccce7f2129aa93a3127d0ae7f0
Eko Andri Subarnanto <eko.andri@icloud.com>
2019-09-14 11:08:01 +0700

    Build error {} reducer :((

--- pages/utils/reducers.js
+++ pages/utils/reducers.js
@@ -1,79 +1,72 @@
-function buttonsReducer(_, action) {
+function buttonsReducer(state, action) {
   // console.log(action);
   const { type, refetch, previous, next } = action;
   switch (type) {
     case "INIT":
-      return {
-        pokemon: [
-          {
-            key: 1,
-            title: "Previous",
-            type: "left",
-            disabled: true
-          },
-          { key: 2, title: "Next", type: "right", disabled: false }
-        ]
-      };
-    case "BUTTONS":
-      if (previous === null) {
-        return {
-          pokemon: [
-            {
-              key: 1,
-              title: "Previous",
-              type: "left",
-              disabled: true
-            },
-            {
-              key: 2,
-              title: "Next",
-              type: "right",
-              disabled: false,
-              onclick: () => refetch(next)
-            }
-          ]
-        };
-      } else if (next === null) {
-        return {
-          pokemon: [
-            {
-              key: 1,
-              title: "Previous",
-              type: "left",
-              disabled: false,
-              onclick: () => refetch(previous)
-            },
-            {
-              key: 2,
-              title: "Next",
-              type: "right",
-              disabled: true
-            }
-          ]
-        };
-      } else {
-        return {
-          pokemon: [
-            {
-              key: 1,
-              title: "Previous",
-              type: "left",
-              disabled: false,
-              onclick: () => refetch(previous)
-            },
-            {
-              key: 2,
-              title: "Next",
-              type: "right",
-              disabled: false,
-              onclick: () => refetch(next)
-            }
-          ]
-        };
-      }
+      return [
+        {
+          key: 1,
+          title: "Previous",
+          type: "left",
+          disabled: true
+        },
+        { key: 2, title: "Next", type: "right", disabled: false }
+      ];
+    case "PREVIOUS_NULL":
+      return [
+        {
+          key: 1,
+          title: "Previous",
+          type: "left",
+          disabled: true
+        },
+        {
+          key: 2,
+          title: "Next",
+          type: "right",
+          disabled: false,
+          onclick: () => refetch(next)
+        }
+      ];
+    case "NEXT_NULL":
+      return [
+        {
+          key: 1,
+          title: "Previous",
+          type: "left",
+          disabled: false,
+          onclick: () => refetch(previous)
+        },
+        {
+          key: 2,
+          title: "Next",
+          type: "right",
+          disabled: true
+        }
+      ];
+    case "NOT_NULL":
+      return [
+        {
+          key: 1,
+          title: "Previous",
+          type: "left",
+          disabled: false,
+          onclick: () => refetch(previous)
+        },
+        {
+          key: 2,
+          title: "Next",
+          type: "right",
+          disabled: false,
+          onclick: () => refetch(next)
+        }
+      ];
+
     default:
       throw new Error();
   }
 }
-
-export { buttonsReducer };
+// export default function JustInCase() {
+//   return <h1>YO</h1>;
+// }
+export default buttonsReducer;
